5307136ff45c772551083518514d78be2a55d903
cleverAkanimoh <crushclever1@gmail.com>
2023-03-14 12:42:45 +0100

    modified App.jsx in src

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
   store.subscribe(() => {
     console.log("store changed", store.getState());
   })
+  
   store.dispatch(actionsCreator("bug 1"))
 
   // store.dispatch()
